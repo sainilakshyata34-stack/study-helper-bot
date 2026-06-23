ae57a2645c3da655e9bd1fecb499f3b69ed61c0e
sainilakshyata34-stack <sainilakshyata34@gmail.com>
2026-06-23 10:01:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pypdf import PdfReader
 # =========================
 # Gemini API Key
 # =========================
-client = genai.Client(api_key="YOUR_ACTUAL_GEMINI_API_KEY")
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 # =========================
 # Page Settings
 # =========================
